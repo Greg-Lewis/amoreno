8296d1ea5d32b8265113bf19251c18f9f63bf568
Greg Lewis <ghlkvf@gmail.com>
2024-11-13 21:03:29 -0700

    commit to put in the basics

---
+++ script.js
@@ -0,0 +1,19 @@
+// forward slashes let you comment things
+function suggestFood() {
+    const foodList = document.getElementById('food-list');
+    const suggestionOutput = document.getElementById('suggestion-output');
+
+    const selectedFood = foodList.value;
+
+    const suggestions = {
+        pizza: ["Pepperoni Pizza", "Margherita Pizza", "Veggie Pizza", "BBQ Chicken Pizza"],
+        burger: ["Cheeseburger", "Veggie Burger", "Bacon Burger", "Chicken Burger"]
+    };
+
+    if (selectedFood) {
+        const randomIndex = Math.floor(Math.random() * suggestions[selectedFood].length);
+        suggestionOutput.textContent = `How about trying ${suggestions[selectedFood][randomIndex]}?`;
+    } else {
+        suggestionOutput.textContent = "Please select a food to get a suggestion.";
+    }
+}
