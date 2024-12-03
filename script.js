60591131f678f82f2f8d60422ae4111fc4ee9f86
arely-moreno <arelymoreno1331@gmail.com>
2024-12-02 20:41:55 -0700

    added a button, added a loop, added a function to button

--- script.js
+++ script.js
@@ -5,15 +5,34 @@ function suggestFood() {
 
     const selectedFood = foodList.value;
 
-    const suggestions = {
+    var suggestions = {
         pizza: ["Pepperoni Pizza", "Margherita Pizza", "Veggie Pizza", "BBQ Chicken Pizza"],
         burger: ["Cheeseburger", "Veggie Burger", "Bacon Burger", "Chicken Burger"],
         tacos: ["Beef Tacos", "Chicken Tacos", "Pork Tacos", "Veggie Tacos"]
     };
 
+
     if (selectedFood) {
+
+        for ( var key in localStorage) { 
+            var storage = suggestions[selectedFood].indexOf(key); 
+            if (storage != -1) {
+                suggestions[selectedFood].splice(storage, 1);
+
+            }
+        }
+
         const randomIndex = Math.floor(Math.random() * suggestions[selectedFood].length);
-        suggestionOutput.textContent = `How about trying ${suggestions[selectedFood][randomIndex]}?`;
+        const displayedFood = suggestions[selectedFood][randomIndex];
+        suggestionOutput.textContent = "How about trying " + displayedFood + "?";
+        var saveFoodEaten = document.createElement('button');
+        saveFoodEaten.textContent = 'Food Eaten!';
+        saveFoodEaten.onclick = function() {
+            localStorage.setItem(displayedFood, "");
+            suggestFood();
+
+        };
+        suggestionOutput.appendChild(saveFoodEaten);
     } else {
         suggestionOutput.textContent = "Please select a food to get a suggestion.";
     }
